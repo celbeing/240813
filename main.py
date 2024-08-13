92ca2a3501714f56aa352b43c1ae73410e9b0813
전라남도교육지원청 <76736721+celbeing@users.noreply.github.com>
2024-08-13 15:51:22 +0900

    Update main.py
    타이틀 수정

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import settings
 #load_dotenv()
 
 # python -m streamlit run main.py
-st.title("📝석리송 전용 GPT(테디노트 이경록 대표님 코드 기반)")
+st.title("📝전라남도교육지원청 전용 GPT(테디노트 이경록 대표님 코드 기반)")
 
 config = settings.load_config()
 if "api_key" in config:
